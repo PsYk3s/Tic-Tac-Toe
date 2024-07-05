93d2341006e5e6f651687c37eb18fe6e83ea4be3
Warren <the.waz88@gmail.com>
2024-07-05 12:03:41 +0200

    Pseudocoding and planning script file

---
+++ script.js
@@ -0,0 +1,24 @@
+//click start -> prompt for player 1 and player 2 -> board becomes active -> start changes to reset
+
+//Module
+
+    //Factory
+    //Generate game board, player names, scores, score adding function
+
+//Store variables, request factory interactions, output current status
+
+const gameObj = [
+    {
+        gameBoard : [],
+        winConditionRef : [],
+        gameInPlay : false,
+        turn : 1,
+    },
+    {
+        player1 : "",
+        player2 : "",
+        player1Score : 0,
+        player2Score : 0,
+    },
+];
+
